cd53ef2919e65733b93edbeaf18b5a545aa7ed80
leonardo <sigma.leonnard@gmail.com>
2019-07-11 21:23:17 -0300

    first

---
+++ app/src/main/java/android/festafimdeano/detailsActivity.java
@@ -0,0 +1,14 @@
+package android.festafimdeano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
