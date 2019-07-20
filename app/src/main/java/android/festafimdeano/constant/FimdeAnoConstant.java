8a6c6b12d05bedf9c5a53c1c42ab168c8f285d4c
nexoZero <sigma.leonnard@gmail.com>
2019-07-20 15:23:27 -0300

    add classe de constantes

---
+++ app/src/main/java/android/festafimdeano/constant/FimdeAnoConstant.java
@@ -0,0 +1,7 @@
+package android.festafimdeano.constant;
+
+public class FimdeAnoConstant {
+    public static String PRENSENCE_KEY = "presenceKey";
+    public static String PRENSENCE_YES = "presenceYes";
+    public static String PRENSENCE_NO = "presenceNo";
+}
